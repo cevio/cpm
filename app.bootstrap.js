f2bd3411f759bbfede71cd4fb1a52782e80f79ef
evio <evio@vip.qq.com>
2018-11-12 11:24:38 +0800

    update

--- app.bootstrap.js
+++ app.bootstrap.js
@@ -1,8 +1,5 @@
 const mount = require('koa-mount');
 module.exports = async app => {
-  if (!app.context.Service.Authorization) {
-    throw new Error('can not find the Authorization, you must code it first.');
-  }
   app.use(mount('/download', app.Middleware.Static(app.config.nfs)));
   app.use(async (ctx, next) => {
     ctx.onResponseError(error => {
