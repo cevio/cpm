3c172607c881b5200065da40e72d0edf24da9ce5
evio <evio@vip.qq.com>
2018-11-12 14:53:12 +0800

    change formdata size

--- app/middleware/body.js
+++ app/middleware/body.js
@@ -2,5 +2,6 @@ const Convert = require('koa-convert');
 const Body = require('koa-better-body');
 module.exports = Convert(Body({
   fields: 'body',
-  files: 'files'
+  files: 'files',
+  jsonLimit: '1mb'
 }));
