4fe205d9d33c46fd5381abd4e91538176b153df5
evio <evio@vip.qq.com>
2018-11-19 15:08:37 +0800

    update

--- app.bootstrap.js
+++ app.bootstrap.js
@@ -3,6 +3,9 @@ const mount = require('koa-mount');
 const static = require('koa-static');
 const WebResourcePath = path.resolve(__dirname, 'web');
 module.exports = async app => {
+  if (!app.Service.Authorization) {
+    throw new Error('You should setup your own `UserService` first');
+  }
   app.use(static(WebResourcePath, {
     gzip: true
   }));
